2163746bc124f6ddbf52c9689bd1f23ce67034cf
Alex Hu <alexhu98@gmail.com>
2020-08-02 12:43:30 -0700

    Added page exit animation

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,24 +1,113 @@
-import React, { PropsWithChildren, useState, useEffect } from 'react'
+import * as R from 'ramda'
+import React, { PropsWithChildren, useEffect, useState } from 'react'
+import { GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
+import { useQuery } from '@apollo/react-hooks'
+import { initializeApollo } from 'apollo/client'
+import { ArticlesQuery } from 'apollo/queries'
 import Head from 'next/head'
+import { useAnimation } from 'framer-motion';
+import { motion, PanInfo } from 'framer-motion'
 import { NavBar } from './Navigation'
 import Footer from './Footer'
+import { ArticlesResult, IArticle } from 'models/article'
 
 type Props = {
   title: string
   activeItem: string
 }
 
+const buildRoutes = (articles: IArticle[] | undefined): string[] => {
+  // console.log(`buildRoutes() articles`, articles)
+  const routes = R.pipe(
+    R.defaultTo([]),
+    // @ts-ignore
+    R.filter((article: IArticle) => ['Sprints', 'Posts'].includes(article.section)),
+    // @ts-ignore
+    R.sortBy(R.prop('createdAt')),
+    R.map(article => `/posts/${article.id}`),
+    R.concat(['/', '/posts'])
+  // @ts-ignore
+  )(articles)
+  // console.log(`buildRoutes routes`, routes)
+  return routes
+}
+
+const getNextRoute = (path: string, routes: string[], direction: number): string => {
+  // console.log(`getNextRoute.routes`, routes)
+  let routeIndex = routes.indexOf(path) + direction
+  if (routeIndex < 0) {
+    routeIndex = routes.length - 1
+  }
+  else if (routeIndex >= routes.length) {
+    routeIndex = 0
+  }
+  const nextRoute = routes[routeIndex]
+  // console.log(`getNextRoute.routeIndex`, routeIndex)
+  // console.log(`getNextRoute.nextRoute`, nextRoute)
+  return nextRoute || '/'
+}
+
 const Layout: React.FC<PropsWithChildren<Props>> = ({ children, title, activeItem }) => {
-  // avoid Flash of Unstyled Content by hiding all contents initially
-  // then make it visible once loaded
-  const [foucClassName, setFoucClassName] = useState('fouc')
+
+  const router = useRouter()
+  const exitAnimation = useAnimation()
+  const { data } = useQuery<ArticlesResult>(ArticlesQuery)
+  const [routes, setRoutes] = useState(() => buildRoutes(data?.articles))
+  const [backHref, setBackHref] = useState(() => getNextRoute(router.asPath, routes, -1))
+  const [nextHref, setNextHref] = useState(() => getNextRoute(router.asPath, routes, 1))
 
   useEffect(() => {
-    setFoucClassName('')
-  }, [])
+    const newRoutes = buildRoutes(data?.articles)
+    setRoutes(newRoutes)
+    setBackHref(getNextRoute(router.asPath, newRoutes, -1))
+    setNextHref(getNextRoute(router.asPath, newRoutes, 1))
+  }, [data])
+
+  const back = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if (!e.ctrlKey) {
+      e.preventDefault()
+      await exitAnimation.start('exitBack')
+      router.push(backHref)
+    }
+}
+
+  const next = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if (!e.ctrlKey) {
+      e.preventDefault()
+      await exitAnimation.start('exitNext')
+      router.push(nextHref)
+      }
+  }
+
+  // avoid Flash of Unstyled Content by hiding all contents initially
+  // then make it visible once loaded
+  const containerVariants = {
+    hidden: {
+      opacity: 0
+    },
+    visible: {
+      opacity: 1,
+      transition: { duration: 0.1 },
+    },
+    exitBack: {
+      x: '100vw',
+      transition: { ease: 'easeInOut' },
+      duration: 1,
+    },
+    exitNext: {
+      x: '-100vw',
+      transition: { ease: 'easeInOut' },
+      duration: 1,
+    },
+  }
 
   return (
-    <div className={foucClassName}>
+    <motion.div
+      variants={containerVariants}
+      initial='hidden'
+      animate='visible'
+    >
       <Head>
         <title>{ title }</title>
         <meta charSet='utf-8' />
@@ -27,11 +116,33 @@ const Layout: React.FC<PropsWithChildren<Props>> = ({ children, title, activeIte
         <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap' />
         <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400&display=swap' />
       </Head>
-      <NavBar activeItem={activeItem} />
-      {children}
-      <Footer />
-    </div>
+      <NavBar activeItem={activeItem} backHref={backHref} nextHref={nextHref} onClickBack={back} onClickNext={next} />
+      <motion.div
+        variants={containerVariants}
+        animate={exitAnimation}
+      >
+        {children}
+        <Footer />
+      </motion.div>
+    </motion.div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const apolloClient = initializeApollo()
+  try {
+    await apolloClient.query({
+      query: ArticlesQuery,
+    })
+  }
+  catch (error) {
+    console.error('Posts -> getStaticProps -> error', error)
+  }
+  return {
+    props: {
+      initialApolloState: apolloClient.cache.extract(),
+    },
+  }
 }
 
 export default Layout
---
+++ docs/Posts/20200720-a-touch-of-animation.md
@@ -0,0 +1,130 @@
+# A Touch of Animation
+##### July 20th, 2020 By Alex Hu
+
+[Framer Motion](https://www.framer.com/motion/) is a animation library for React. This library is rather easy to use.
+Instead of a <&nbsp;h2&nbsp;>, you can use <&nbsp;motion.h2&nbsp;> and add the animation as props.
+
+I added some subtle animations to the pages.
+
+### [Timeline](/)
+
+In the timeline, the link for article title is scaled slightly larger when hovering. The bulk of the work is to
+find what the Link element actually mapped to as an <&nbsp;a&nbsp;> element and then put in the right class names. The actual
+animation is a simple whileHover property.
+
+```js
+whileHover={{
+  scale: 1.1,
+  originX: 0,
+}}
+>
+```
+
+### [Article Card](/posts)
+
+The second hover effect is the on the box shadow of the article card and a very slight scaling of it,
+by a simple replacement of the <&nbsp;Card&nbsp;> component. You hardly notice that the animation because of the
+very short duration but it is there.
+
+```js
+<motion.div
+  className={clsx('article-card', 'large', 'MuiPaper-root MuiCard-root MuiPaper-outlined MuiPaper-rounded')}
+  whileHover={{
+    scale: 1.01,
+    boxShadow: '0px 6px 6px -4px rgba(0,0,0,0.2), 0px 6px 8px 0px rgba(0,0,0,0.14), 0px 2px 16px 0px rgba(0,0,0,0.12)',
+  }}
+  transition={{
+    duration: 0.05,
+  }}
+>
+```
+
+### [Flash of Unstyled Content](/posts/flash-of-unstyled-content)
+
+I replaced the old FOUC mechanism with a simple fade in effect on a layout wrapper.
+
+```js
+import React, { PropsWithChildren } from 'react'
+import Head from 'next/head'
+import { motion } from 'framer-motion'
+import { NavBar } from './Navigation'
+import Footer from './Footer'
+
+type Props = {
+  title: string
+  activeItem: string
+}
+
+const Layout: React.FC<PropsWithChildren<Props>> = ({ children, title, activeItem }) => {
+  // avoid Flash of Unstyled Content by hiding all contents initially
+  // then make it visible once loaded
+  const containerVariants = {
+    hidden: {
+      opacity: 0
+    },
+    visible: {
+      opacity: 1,
+      transition: { duration: 0.1 },
+    },
+  }
+
+  return (
+    <motion.div
+      variants={containerVariants}
+      initial='hidden'
+      animate='visible'
+    >
+      <Head>
+        <title>{ title }</title>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta name='google-site-verification' content='yKJNmNpvtiHvsXH_CN5BxIgVy5dwktxYqpXYAQgvNdo' />
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap' />
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400&display=swap' />
+      </Head>
+      <NavBar activeItem={activeItem} />
+      {children}
+      <Footer />
+    </motion.div>
+  )
+}
+
+export default Layout
+```
+
+### Exit Animation
+
+I added the left / right arrow buttons at the top right corner for easy navigation to the previous / next article.
+When clicked, it will trigger the exit animation by swiping the article left / right.
+
+```js
+const containerVariants = {
+  exitBack: {
+    x: '100vw',
+    transition: { ease: 'easeInOut' },
+    duration: 1,
+  },
+  exitNext: {
+    x: '-100vw',
+    transition: { ease: 'easeInOut' },
+    duration: 1,
+  },
+}
+
+const exitAnimation = useAnimation()
+
+const back = async () => {
+  await exitAnimation.start('exitBack')
+  router.push(getNextRoute(router.route, routes, -1))
+}
+
+const next = async () => {
+  await exitAnimation.start('exitNext')
+  router.push(getNextRoute(router.route, routes, 1))
+}
+
+<motion.div
+  variants={containerVariants}
+  animate={exitAnimation}
+>
+```
